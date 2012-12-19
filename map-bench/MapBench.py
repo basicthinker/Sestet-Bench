6e784910709d781d2c811c9d0cc6ff4a6fbfeba2
Jinglei Ren <jinglei.ren@gmail.com>
2012-12-20 01:56:02 +0800

    [MapBench] Realize one trial of image recording/comparison.

--- map-bench/MapBench.py
+++ map-bench/MapBench.py
@@ -1,39 +1,89 @@
 # Imports the monkeyrunner modules used by this program
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
+APK_PATH = 'C:\\Users\\basicthinker\\Downloads\\com.baidu.BaiduMap-1.apk'
+IMAGE_PATH = 'C:\\Users\\basicthinker\\Downloads\\'
+PKG_NAME = 'com.baidu.BaiduMap'
+ACTIVITY = 'com.baidu.BaiduMap.map.mainmap.MainMapActivity'
+
 # Connects to the current device, returning a MonkeyDevice object
 device = MonkeyRunner.waitForConnection()
 # retrives basic properties
 width = int(device.getProperty("display.width"))
 height = int(device.getProperty("display.height"))
 
-# sets a variable with the package's internal name
-package = 'com.baidu.BaiduMap'
-# sets a variable with the name of an Activity in the package
-activity = 'com.baidu.BaiduMap.map.mainmap.MainMapActivity'
-# sets the name of the component to start
-runComponent = package + '/' + activity
+# Wrapper function for touching a point down and up
+def touchDownUp(x_rate, y_rate, interval = 0.5):
+    MonkeyRunner.sleep(interval)
+    x = int(width * x_rate)
+    y = int(height * y_rate)
+    device.touch(x, y, MonkeyDevice.DOWN_AND_UP)
+    return
 
-# Runs the component
-device.startActivity(component=runComponent)
-MonkeyRunner.sleep(0.5)
-# device.shell("adb shell am start -n com.baidu.BaiduMap/com.baidu.BaiduMap.map.mainmap.MainMapActivity");
+NUM_SNAP = 10
+targets = [None] * NUM_SNAP # [0]origin, [1]zoom-in 1, [2]zoom-in 2, [3]zoom-in 3,
+                            # [4]move-right, [5]move-down, [6]move-left, [7]move-up
+                            # [8]zoon-out 1, [9]zoom-out 2, [10]zoom-out 3
 
-# Touches the input box
-x = int(width * 0.4)
-y = int(height * 0.08)
-device.touch(x, y, MonkeyDevice.DOWN_AND_UP)
-MonkeyRunner.sleep(1)
+def takeSnapshot():
+    y = height * 0.05
+    h = height * 0.90
+    return device.takeSnapshot().getSubImage((0, y, width, h))
 
-# Inputs place and search
-device.type("Tsinghua")
-x = int(width * 0.9)
-y = int(height * 0.17)
-device.touch(x, y, MonkeyDevice.DOWN_AND_UP)
+def recordTarget(index, interval = 10):
+    MonkeyRunner.sleep(interval)
+    targets[index] = takeSnapshot()
+    MonkeyRunner.sleep(interval)
+    state = takeSnapshot()
+    while (not targets[index].sameAs(state)):
+        targets[index] = state
+        MonkeyRunner.sleep(interval)
+        state = takeSnapshot()
+    state.writeToFile(IMAGE_PATH + 'MapBenchSnapshot' + str(index) + '.png') 
+    return
 
-# Initial image
-MonkeyRunner.sleep(10)
-snapshot = device.takeSnapshot()
+def finishOperation(index, threshod = 0.5, interval = 5):
+    MonkeyRunner.sleep(interval)
+    state = takeSnapshot()
+    while (not targets[index].sameAs(state, threshod)):
+        state.writeToFile(IMAGE_PATH + 'MapBenchSnapshot' + str(index) + 'x.png')
+        MonkeyRunner.sleep(interval)
+        state = takeSnapshot()
+    return
 
-# Presses the Menu button
-# device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
+def operateMap(init=False):
+    # Launches the app
+    runComponent = PKG_NAME + '/' + ACTIVITY
+    device.startActivity(component=runComponent)
+    # accepts TOS
+    touchDownUp(0.3, 0.85)
+    # closes welcome
+    touchDownUp(0.96, 0.07)
+    # cancels further setting
+    touchDownUp(0.76, 0.48)
+    # turns off the real traffic
+    touchDownUp(0.92, 0.17, 1)
+    # touches the input box
+    touchDownUp(0.4, 0.08)
+    # inputs place
+    MonkeyRunner.sleep(3)
+    device.type("Tsinghua")
+
+    touchDownUp(0.9, 0.17)
+    index = 0; # [0] origin
+    if init:
+        recordTarget(index)
+    else:
+        finishOperation(index)
+    return
+
+# Main
+device.removePackage(PKG_NAME)
+device.installPackage(APK_PATH)
+operateMap(True)
+
+device.removePackage(PKG_NAME)
+device.installPackage(APK_PATH)
+operateMap()
+
+
