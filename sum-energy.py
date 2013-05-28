d4713d41015814ad4e284431e8954fefeb6a3cae
Jinglei Ren <jinglei.ren@gmail.com>
2013-05-28 22:02:37 +0800

    Refine sum-energy.py and remove install packages.

--- sum-energy.py
+++ sum-energy.py
@@ -15,28 +15,32 @@ def fetch_on(fp, start, end):
       continue
     if time >= end:
       break
-    mA = float(segments[1])
-    V = float(segments[2])
-    sum += mA * V;
+    mW = float(segments[1])
+    sum += mW;
   
   sum *= INTERVAL
   return sum
 
 # Main
-if len(sys.argv) != 3:
-  print "Usage: python %s LogFile IntervalFile" % sys.argv[0]
+if len(sys.argv) != 4:
+  print "Usage: python %s LogFile IntervalFile OutputColumnNum" % sys.argv[0]
   sys.exit(-1)
 
 log = sys.argv[1]
 intervals = sys.argv[2]
+columns = int(sys.argv[3])
 
 log_file = open(log, 'r')
 int_file = open(intervals, 'r')
 
 line = log_file.readline() # skips the field names
+index = 0;
 for line in int_file:
   segments = string.split(line, '\t')
   start = float(segments[0])
   end = float(segments[1])
-  print fetch_on(log_file, start, end)
-
+  index += 1
+  if index % columns == 0:
+    print fetch_on(log_file, start, end)
+  else:
+    print "%f\t" % fetch_on(log_file, start, end),
