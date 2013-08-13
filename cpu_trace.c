2a4308dbadadc54857eb4bb383cbe74c94a6ed11
Jinglei Ren <jinglei.ren@gmail.com>
2013-08-13 18:02:14 +0800

    Add no-chart option to new cpu_trace.c.

--- cpu_trace.c
+++ cpu_trace.c
@@ -3,21 +3,20 @@
 #include "monitor.h"
 
 int main(int argc, char *argv[]) {
-  int num_smpl, uslice;
+  int num_smpl, is_chart, uslice;
   struct timeval tv;
   double time;
   struct cpu_stat cpu_s;
   double cpu_u;
   int len, i;
 
-  time = get_time(&tv);
-
-  if (argc != 2) {
-    printf("Usage: %s SamplesPerSec\n", argv[0]);
+  if (argc != 3) {
+    printf("Usage: %s SamplesPerSec ToShowCharts\n", argv[0]);
     return -1;
   }
 
   num_smpl = atoi(argv[1]);
+  is_chart = atoi(argv[2]);
   uslice = 1000000 / num_smpl;
 
   cpu_util_init(&cpu_s);
@@ -25,12 +24,14 @@ int main(int argc, char *argv[]) {
     usleep(uslice);
     cpu_u = cpu_util(&cpu_s);
     len = cpu_u / 0.02;
-    printf("%.2f%%\t", cpu_u * 100);
-    for (i = 0; i < len; ++i)
-      putchar('+');
-    for (i = 50 - len; i > 0; --i)
-      putchar('-');
-    printf("\t%.2f\n", get_time(&tv) - time);
+    printf("%.6f\t%.2f%%\t", get_time(&tv), cpu_u * 100);
+    if (is_chart) {
+      for (i = 0; i < len; ++i)
+        putchar('+');
+      for (i = 50 - len; i > 0; --i)
+        putchar('-');
+    }
+    putchar('\n');
   }
 
   return 0;
