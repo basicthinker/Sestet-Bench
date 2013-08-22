544baa9b07e07af261a3fd2751b37a13571c1621
Jinglei Ren <jinglei.ren@gmail.com>
2013-08-22 09:19:07 +0800

    [utrace] pre_trace.sh works on host.

--- utrace/pre_trace.sh
+++ utrace/pre_trace.sh
@@ -10,10 +10,11 @@ fi
 
 app_alias=$1
 out_dir=$2
+mkdir -p $2
 timestamp=`date +"%F-%T"`
 
 adb shell "su -c '$SH_DIR/prepare_in_dev.sh $DATA_DIR/$app_alias.part $DATA_DIR/rffs.ko'"
-adb shell "su -c '$SH_DIR/ev_trace.o'" > $app_alias-ev-$timestamp.log &
-adb shell "su -c 'cat /proc/kmsg | grep rffs'" > $app_alias-kern-$timestamp.log &
-adb shell "su -c 'echo "[rffs] begin time" > /dev/kmsg; $SH_DIR/cpu_trace.o 5 0'" > $app_alias-cpu-$timestamp.log &
-cat $app_alias-cpu-$timestamp.log
+adb shell "su -c '$SH_DIR/ev_trace.o'" > $out_dir/$app_alias-ev-$timestamp.log &
+adb shell "su -c 'cat /proc/kmsg | grep rffs'" > $out_dir/$app_alias-kern-$timestamp.log &
+adb shell "su -c 'echo '[rffs] begin time' > /dev/kmsg; $SH_DIR/cpu_trace.o 5 0'" > $out_dir/$app_alias-cpu-$timestamp.log &
+cat $out_dir/$app_alias-cpu-$timestamp.log
